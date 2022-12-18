f392a24fe015a1b17698aaee98deebe5ce8d0068
EddieHooks36 <eddiehooks36@gmail.com>
2022-12-18 09:40:21 -0800

    pet salon object literal

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,8 @@
+//object literal
+//name, age, gender, breed , service, owners, phone
 let salon = {
     name:"The Fashion Pet",
+    phone: "111-111-1111",
     address: {
         street: "Fake Street",
         zip: 12345,
@@ -8,8 +11,34 @@ let salon = {
     hours:{
         open: "9:00am",
         close: "5:00pm"
-    }
+    },
+    pets:[
+        {   //anonymous object
+            name:"Scooby",
+            age:50,
+            gender:"male",
+            breed: "Dane",
+            service:"Grooming",
+            contactphone:"111-111-1111"
+        },
+        {
+            name:"Gigi",
+            age:12,
+            gender: "Female",
+            breed: "Rotwhieler",
+            service: "Grooming",
+            contactphone:"111-111-1111"
+        },
+    ]
     
 }
 
-console.log(salon);
+function displayifo(){
+
+}
+
+function init(){
+    console.log(Salon,pets[0].name)
+}
+
+window.onload = init;
